e2b5cd0ed847194549366e422d8adbd80dea1f95
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-06-15 17:30:33 +0900

    Storybookボタン呼び出し

--- app/static_page_sample/page.tsx
+++ app/static_page_sample/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { Button } from "@/stories/Button";
+
 export default function hoge() {
   return (
     <main>
       <p>hogehoge</p>
+      <Button label={"hossssssssge"}></Button>
     </main>
   );
 }
