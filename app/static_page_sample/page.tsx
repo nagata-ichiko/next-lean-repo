6b37c63b8ec7e6a98dcefc24a26f3a55b54a81a8
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-06-16 16:06:44 +0900

    Mterialui追加

--- app/static_page_sample/page.tsx
+++ app/static_page_sample/page.tsx
@@ -1,11 +1,15 @@
 "use client";
-import { Button } from "@/stories/Button";
+// import { Button } from "@/stories/Button";
+import { Button } from "@mui/material";
 
 export default function hoge() {
   return (
     <main>
       <p>hogehoge</p>
-      <Button label={"hossssssssge"}></Button>
+      {/* <Button label={"hossssssssge"}></Button> */}
+      <Button>text</Button>
+      <Button variant="contained">contained</Button>
+      <Button variant="outlined">outlined</Button>
     </main>
   );
 }
