0ca29ba4e86a3b384988e2920cb0f02a2b3b8dc1
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2023-09-05 10:32:06 +0900

    バックエンド動いた

--- stories/sample/ListItem.stories.tsx
+++ stories/sample/ListItem.stories.tsx
@@ -1,48 +1,48 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { ListItem } from "./ListItem";
+import { List } from "./List";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-const meta: Meta<typeof ListItem> = {
-  title: "Example/ListItem",
-  component: ListItem,
+const meta: Meta<typeof List> = {
+  title: "Example/List",
+  component: List,
   tags: ["autodocs"],
 };
 
 export default meta;
-type Story = StoryObj<typeof ListItem>;
+type Story = StoryObj<typeof List>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const None: Story = {
+export const Menbder: Story = {
   args: {
-    status: `none`,
-    label: "Button",
+    header: [`名前`, `年齢`, `性別`],
+    body: [
+      {
+        row: [`山田太郎`, `20`, `男`],
+      },
+      {
+        row: [`山田太郎`, `20`, `男`],
+      },
+      {
+        row: [`山田太郎`, `20`, `男`],
+      },
+    ],
   },
 };
 
-export const Error: Story = {
+export const Infomation: Story = {
   args: {
-    status: `none`,
-    label: "Button",
-  },
-};
-
-export const Alert: Story = {
-  args: {
-    label: "Button",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button",
+    header: [`電話番号`, `住所`, `備考`],
+    body: [
+      {
+        row: [`090-1234-5678`, `東京都`, `特になし`],
+      },
+      {
+        row: [`090-1234-5678`, `東京都`, `特になし`],
+      },
+      {
+        row: [`090-1234-5678`, `東京都`, `特になし`],
+      },
+    ],
   },
 };
